7952f4b56a085c08cbe48afb9c0695f51624c681
Marcelo Santos <masantos.itlean@sbt.com.br>
2020-05-01 15:44:56 -0300

    Configure Spring Profile for Local MySQL

--- src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
+++ src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
@@ -20,26 +20,34 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.sql.Timestamp;
 import java.util.UUID;
 
-/** Created by jt on 2019-01-26. */
+/**
+ * Created by jt on 2019-01-26.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
 public class BeerInventory extends BaseEntity {
 
+  @Type(type = "org.hibernate.type.UUIDCharType")
+  @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
   private UUID beerId;
+
   private String upc;
   private Integer quantityOnHand = 0;
+
   @Builder
   public BeerInventory(
-      UUID id,
-      Long version,
-      Timestamp createdDate,
+          UUID id,
+          Long version,
+          Timestamp createdDate,
       Timestamp lastModifiedDate,
       UUID beerId,
       String upc,
